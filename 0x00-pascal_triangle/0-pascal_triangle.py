60973b89662cae3c21c72288709e1ef2cd5cdf6b
Tajudeen <lwltjdn@gmail.com>
2024-07-13 18:13:17 +0100

    Updated the codes and documented them properly.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,15 +9,26 @@ def pascal_triangle(n):
         Returns pascal's trangle(list of lists of integers)
         or an empty list if n <= 0
     '''
-    if n <= 0:
+    if n <= 0: #  validates the value of n
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        trngl = triangles[-1]
-        tmp = [1]
-        for i in range(len(trngl) - 1):
-            tmp.append(trngl[i] + trngl[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+    # initialize triangle with a single row [1]
+    triangle = [[1]]
+
+    # Loop hrough to generate the rows
+    while len(triangle) != n:
+        # get the last row
+        last_row = triangle[-1]
+        # Create a new row with the first value = 1
+        new_row = [1]
+        # Compute the immediate elements of the new row
+        for count in range(len(last_row) - 1):
+            # using two consecutive elements from the last row
+            # to compute new immediate element for the new row
+            new_row.append(last_row[count] + last_row[count + 1])
+        # add / appeend 1 to the end of the new row
+        new_row.append(1)
+        # add / append new row to triangle
+        triangle.append(new_row)
+    # return trinagle
+    return triangle
